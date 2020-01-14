fc56d98964cf6a04e487d2edd7ea5aa1da1738bd
Steffen Wendzel <wendzel@hs-worms.de>
2020-01-14 17:14:05 +0100

    add function to scan for bad usernames that regularly appear via SSH brute-force

--- hlhs/postinst.sh
+++ hlhs/postinst.sh
@@ -6,6 +6,12 @@ else
 	chmod 744 etc/rc.d/rc.hlhs
 fi
 
+if [ -f etc/hlhs_bad.users.conf.new ]; then
+	chmod 744 etc/hlhs_bad.users.conf.new
+else
+	chmod 744 etc/hlhs_bad.users.conf
+fi
+
 chmod 700 usr/sbin/hlhs
 
 
